9126b9227a38009eab8c2c6e544efcaeaa6c3951
Grigoriy <grikurylev@gmail.com>
2025-11-05 08:38:01 +0300

    ODE

---
+++ Source/ODE.py
@@ -0,0 +1,8 @@
+from Source.functions import z
+
+def Euler(f, x0, y0, a=0, b=1, n=10):
+
+    h = (b-a)/n # h = 0.1
+    while a<=x0<=b:
+        delta_y0 = h*y_pr
+        y_pr = delta_y0/delta_x0
--- Source/functions.py
+++ Source/functions.py
@@ -1,7 +1,14 @@
 import numpy as np
 
+# Численное интегрирование
 def f(x):
     return np.sqrt(2*x+1.6)/(1.8+np.sqrt(0.3*x**2+2.3))
 
 def g(x,y):
       return (x**2 + y) / (1 + x + y**2)
+
+# Диффернциальное уравнение
+def z(x0=0,y0=1):
+    for i in range(0,1):
+        y_pr=y0*(1-x0)
+    return y_pr
